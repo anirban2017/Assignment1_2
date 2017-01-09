34c44560d548025c23f822ec01287639d2d9725f
anirban2017 <anirbanmit1980@gmail.com>
2017-01-09 09:31:36 +0530

    Add files via upload

---
+++ Assignment1_2.java
@@ -0,0 +1,32 @@
+/*Write a program to print the result of the following expressions provided the integer variable a is
+20 and b is 10.
+int b= a-- - --a;
+int c=a--;
+int d=a>>2;
+int e=a&b;*/
+
+
+public class Assignment1_2 {
+  
+    public static void main(String s[]) 
+{
+
+ int a=20;
+ int b=10;     
+ b= a-- - --a;
+int c=a--;
+int d=a>>2;
+int e=a&b; 
+System.out.println(" Value of b is:- " +b );
+System.out.println(" Value of c is:- " +c );
+System.out.println(" Value of d is:- " +d );
+System.out.println(" Value of e is:- " +e );
+     
+    }  
+  
+    
+    
+}
+
+
+
